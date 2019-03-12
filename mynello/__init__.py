f944524295fdf91ad8aae2ece3834ad9763834f8
JC Heyer <jan-christoph.heyer@transfermarkt.de>
2019-03-12 17:28:58 +0100

    component init

---
+++ mynello/__init__.py
@@ -0,0 +1,14 @@
+
+import logging
+
+# VERSION
+VERSION = '0.1'
+
+# LOGGING
+_LOGGER = logging.getLogger(__name__)
+
+# DOMAIN
+DOMAIN = 'mynello'
+
+# Supported domains
+SUPPORTED_DOMAINS = ['lock']
